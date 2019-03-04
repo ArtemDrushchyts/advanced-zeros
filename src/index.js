74a97df87326c20bf2041d256567c7a71abd4a5e
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-03-04 20:12:35 +0300

    completed task advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  let divisor = [];
+  let pow = [];
+  let count = 0;
+  let index = 0;
+  for(let i = 2; i<=base; i++) {
+    while(base > 1 && base >=i && base%i === 0) {
+      base /= i;
+      count++;
+      divisor[index] = i;
+      pow[index] = count;
+      if(base > 1 && base >=i && base%i != 0) {
+        index++;
+        count = 0;
+      }
+    }
+  }
+
+ let total = [];
+ let sum = 0;
+ let j = 1;
+
+ for(let i = 0; i < divisor.length; i++) {
+   while(number >= Math.pow(divisor[i], j)) {
+    sum += Math.floor(number / Math.pow(divisor[i], j));
+    j++;
+   }
+   total[i] = Math.floor(sum / pow[i]);
+   sum = 0; 
+   j = 1;
+ }
+
+  let min = total[0]; 
+  for(let i = 0; i < total.length; i++) {
+    if(total[i] < min) {
+      min = total[i];
+    }
+  }
+
+  return min;
+
 }
